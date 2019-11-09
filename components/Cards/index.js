f22931efb23bb1f6f36b2d0456ca4a3ad8dd9155
Alessandra Longaretti <tauan@Tauans-iMac.fios-router.home>
2019-11-08 23:58:40 -0500

    Completed MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,106 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+
+// axios.get('https://lambda-times-backend.herokuapp.com/articles')
+// .then(result => {
+//     console.log(result);
+//     article(result);
+
+// })
+
+// .catch(err => {
+//     console.log(err);
+// });
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    // console.log(response);
+    let js = response.data.articles.javascript;
+    const bootstrap = response.data.articles.bootstrap;
+    const technology = response.data.articles.technology;
+    const jquery = response.data.articles.jquery;
+    const node = response.data.articles.node;
+
+
+    js.forEach(item => {
+        entryPoint.appendChild(article(item));
+
+    })
+
+    bootstrap.forEach(item => {
+        entryPoint.appendChild(article(item));
+    })
+
+    technology.forEach(item => {
+        entryPoint.appendChild(article(item));
+    })
+
+    jquery.forEach (item => {
+        entryPoint.appendChild(article(item));
+    })
+
+    node.forEach (item => {
+        entryPoint.appendChild(article(item));
+    })
+})
+
+.catch (error => {
+    console.log(error);
+})
+
+    // Object.keys(response.data.articles).forEach(item =>
+    //     item.forEach(item => {
+    //         entryPoint.appendChild(article(item))
+    //     })
+    // })
+    // .catch(error => {
+    //     console.log(error);
+    // })
+
+
+
+
+
+function article (user) {
+
+    const articleCard = document.createElement('div');
+    const articleHead = document.createElement('div');
+    const articleAuthor = document.createElement('div');
+    const articleImgCon = document.createElement('div');
+    const articleImg = document.createElement('img');
+    const articleSpan = document.createElement('span');
+
+    // Class
+
+    articleCard.classList.add('card');
+    articleHead.classList.add('headline');
+    articleAuthor.classList.add('author');
+    articleImgCon.classList.add('img-container');
+
+
+    // Content
+
+    articleImg.src = user.authorPhoto;
+    articleHead.textContent = user.headline;
+    articleSpan.textContent = user.authorname;
+
+
+    // Append 
+
+    articleCard.appendChild(articleHead);
+    articleCard.appendChild(articleAuthor);
+    articleAuthor.appendChild(articleImgCon);
+    articleAuthor.appendChild(articleSpan);
+    articleImgCon.appendChild(articleImg);
+    // document.querySelector('.cards-container').appendChild(articleCard);
+
+    return articleCard;
+
+}
+const entryPoint = document.querySelector('.cards-container');
+
